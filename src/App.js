9ca3f216b55b848d0112354c4c226d025f80326f
lewenweijia <lewenweijia@gmail.com>
2018-03-22 14:35:27 +0800

    get the weather info, DONE

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import { fetchCitysInfo, fetchCityWeatherInfo } from "./utils";
 
 export default class extends Component {
   state = {
     text: "",
-    citys: null
-  }
+    citys: null,
+    adcodes: [],
+    weathers: {}
+  };
 
   componentDidMount() {
-    fetch("http://restapi.amap.com/v3/config/district?key=848da042ee8ec428269dea5432ede9f8&keywords=广东&subdistrict=1&extensions=base")
-      .then(res => res.json())
-      .then(result => result.districts[0].districts)
-      .then(citys => citys.map(city => ({ adcode: city.adcode, name: city.name })))
-      .then(filterCitysInfo => this.setState({citys: filterCitysInfo}))
+    fetchCitysInfo().then(res => this.setState({ citys: res }));
   }
+
+  handleClick = adcode => {
+    if (this.state.adcodes.includes(adcode)) {
+      return this.state.weathers[adcode];
+    }
+    fetchCityWeatherInfo(adcode).then(res => {
+      const weather = res.lives[0];
+      this.setState({ weathers: { [adcode]: { ...weather } } });
+    });
+  };
 
   render() {
     return (
-      <div style={{ marginLeft: 50}}>
-        <h1>广东省市级天气</h1>
+      <div style={{ marginLeft: 50 }}>
+        <h1>广东省市级天气查询</h1>
         <table>
           <thead>
             <tr>
               <th>城市</th>
-              <th>adcode</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.citys ? this.state.citys.map(city => (
-              <tr>
-                <td>{city.name}</td>
-                <td>{city.adcode}</td>
-              </tr>
-            )) : null } 
-      </tbody>
+            {this.state.citys
+              ? this.state.citys.map(city => (
+                  <tr key={city.adcode}>
+                    <td>{city.name}</td>
+                    <td>
+                      <button onClick={() => this.handleClick(city.adcode)}>
+                        Get the Weather
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              : null}
+          </tbody>
         </table>
       </div>
-    )
+    );
   }
 }
---
+++ src/utils.js
@@ -0,0 +1,37 @@
+const API_KEY = "848da042ee8ec428269dea5432ede9f8"
+
+const API_SERVICE_BASE_URL = "http://restapi.amap.com/v3/config/district?"
+
+const options = {
+  key: API_KEY,
+  keywords: "广东",
+  subdistrict: "1",
+  extensions: "base"
+}
+
+let paramsStr = ""
+
+for (let item in options) {
+  paramsStr += `&${item}=${options[item]}`
+}
+
+paramsStr = paramsStr.slice(1)
+
+const resultURL = API_SERVICE_BASE_URL + paramsStr
+
+async function fetchCitysInfo() {
+  const res = await fetch(resultURL)
+  const json = await res.json()
+  const citys = json.districts[0].districts
+  citys.map(city => ({ adcode: city.adcode, name: city.name }))
+  return citys
+}
+
+async function fetchCityWeatherInfo(adcode) {
+  const res = await fetch(`http://restapi.amap.com/v3/weather/weatherInfo?key=${API_KEY}&city=${adcode}`)
+  return await res.json()
+}
+  
+
+export { fetchCitysInfo, fetchCityWeatherInfo }
+
